9537d5b17d03f0ebe6bebea7e072af8eccc55e01
ryanpham540 <ryanpham20@gmail.com>
2024-03-14 16:40:32 -0700

    code as of 3/14/24

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
-window.addEventListener('scroll', function() {
-    var scrollPosition = window.pageYOffset;
-    var speed = 0.5; // Adjust this value to change the speed of the parallax effect
-    var parallaxEffect = scrollPosition * speed;
-
-    document.querySelector('.parallax-section').style.backgroundPosition = 'center ' + parallaxEffect + 'px';
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry);
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
 });
+
+const hiddenElements = document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => observer.observe(el));
